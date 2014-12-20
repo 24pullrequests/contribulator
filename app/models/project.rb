a2eb05d6acd0e8120bdaab4624db93a31a7a92c5
Andrew Nesbitt <andrewnez@gmail.com>
2014-12-20 23:04:44 +0000

    More private methods

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,28 +14,28 @@ class Project < ActiveRecord::Base
     update_score
   end
 
-  def update_from_github
-    update_attributes(
-      github_id:     repo[:id],
-      name:          repo[:name],
-      owner:         repo[:owner][:login],
-      description:   repo[:description],
-      homepage:      repo[:homepage],
-      fork:          repo[:fork],
-      main_language: repo[:language]
-    )
-  end
-
-  def update_score
-    update_attributes score: ScoreCalculator.new(self).score
-  end
-
   def repo_id
     github_id || name_with_owner
   end
 
   private
 
+  def update_from_github
+    update_attributes(
+    github_id:     repo[:id],
+    name:          repo[:name],
+    owner:         repo[:owner][:login],
+    description:   repo[:description],
+    homepage:      repo[:homepage],
+    fork:          repo[:fork],
+    main_language: repo[:language]
+    )
+  end
+
+  def update_score
+    update_attributes score: ScoreCalculator.new(self).score
+  end
+  
   def repo
     @repo ||= Octokit.repo(repo_id)
   end
