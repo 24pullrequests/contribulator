513c900742c0f3e7c45a5581c0134a662c1e2367
Baris Balic <leafybasil@gmail.com>
2014-12-24 23:24:41 +0000

    Routes, controller and views for adding projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,13 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    Project.create_from_github_url(params[:project][:github_url])
+    redirect_to root_path
+  end
 end
