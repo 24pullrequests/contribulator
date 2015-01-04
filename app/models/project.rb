45bb5b66d61f81a155b63c516bc6c39d2e2194c6
Mark Tareshawty <tarebyte@gmail.com>
2015-01-04 13:18:15 -0500

    make sure the url is present

--- app/models/project.rb
+++ app/models/project.rb
@@ -74,7 +74,9 @@ class Project < ActiveRecord::Base
   end
 
   def format_url(url)
-    url[/^https?:\/\//] ? url : "http://#{url}"
+    if url
+      url[/^https?:\/\//] ? url : "http://#{url}"
+    end
   end
 
   def update_score
