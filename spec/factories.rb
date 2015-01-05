c556390444ad0a0fe69a859d743e57bbf40b9e29
Peter Mellett <pjm@petermellett.co.uk>
2015-01-05 21:27:02 +0000

    some specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,18 @@
-FactoryGirl.define do  factory :issue do
-    
+FactoryGirl.define do
+  factory :issue do
+    sequence :github_id do |n|
+      "whatever-#{n}"
+    end
+
+    sequence :url do |n|
+      "http://somewhere-#{n}.com"
+    end
+
+    sequence :title do |n|
+      "#{Faker::Lorem.word} #{n}"
+    end
+
+    project
   end
 
   factory :project do
