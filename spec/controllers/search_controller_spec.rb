090c81afb36338a60edaa37444883c0a91c40d48
Gus Bonfant <gbonfant@me.com>
2015-02-08 00:35:06 +0000

    Refactored Projects controller to use combo search
    The create action will now attempt to find or create a repo given a
    valid github url, otherwise it'll perform a full text search.
    
    Also updated the search button to reflect these changes

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,23 +1,22 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe SearchController, :type => :controller do
-  describe "GET #index" do
-
-    it "responds successfully with an HTTP 200 status code" do
+RSpec.describe SearchController, type: :controller do
+  describe 'GET #index' do
+    it 'responds successfully with an HTTP 200 status code' do
       get :index
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
-    it "renders the projects/index template" do
+    it 'renders the index template' do
       get :index
-      expect(response).to render_template("projects/index")
+      expect(response).to render_template('search/index')
     end
 
-    it "searches for the given query and loads all the projects into @projects" do
-      project = create(:project, name: "pajamas", owner: "bananas")
+    it 'searches for the given query and loads all the projects into @projects' do
+      project = create(:project, name: 'pajamas', owner: 'bananas')
 
-      get :index, q: "bananas"
+      get :index, q: 'bananas'
       expect(assigns(:projects)).to include(project)
     end
   end
