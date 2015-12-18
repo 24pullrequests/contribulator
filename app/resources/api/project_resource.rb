1f4d05b634985dc06c553619bc943a06ff010276
Kris Handley <handley.kris@gmail.com>
2015-12-18 13:41:49 +0000

    Use github ID for the ID in the projects api

--- app/resources/api/project_resource.rb
+++ app/resources/api/project_resource.rb
@@ -7,5 +7,18 @@ module Api
                :name_with_owner,
                :github_id,
                :score
+
+    def id
+      github_id
+    end
+
+    def self.find_by_key(key, options = {})
+      context = options[:context]
+      records = records(options)
+      records = apply_includes(records, options)
+      model = records.find_by(github_id: key)
+      fail JSONAPI::Exceptions::RecordNotFound, key if model.nil?
+      resource_for_model(model).new(model, context)
+    end
   end
 end
