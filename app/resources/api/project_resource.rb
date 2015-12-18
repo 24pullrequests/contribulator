b5ef3f1b5d3c11bc836d9c29c4f6728bebaf4a98
Kris Handley <handley.kris@gmail.com>
2015-12-18 15:01:59 +0000

    Simplify resource ID finding

--- app/resources/api/project_resource.rb
+++ app/resources/api/project_resource.rb
@@ -8,17 +8,8 @@ module Api
                :github_id,
                :score
 
-    def id
-      github_id
-    end
-
-    def self.find_by_key(key, options = {})
-      context = options[:context]
-      records = records(options)
-      records = apply_includes(records, options)
-      model = records.find_by(github_id: key)
-      fail JSONAPI::Exceptions::RecordNotFound, key if model.nil?
-      resource_for_model(model).new(model, context)
+    def self._primary_key
+      :github_id
     end
   end
 end
